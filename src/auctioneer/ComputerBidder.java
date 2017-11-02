e04fc6efc5f5a5cdfa042d601196402b3fcb2a62
Egger <sean.egger@CHAMPLAIN.EDU>
2017-11-02 15:18:02 -0400

    stuff

--- src/auctioneer/ComputerBidder.java
+++ src/auctioneer/ComputerBidder.java
@@ -1,5 +1,18 @@
 package auctioneer;
 
-public class ComputerBidder {
+public class ComputerBidder implements Bidder{
+    private string name;
+    float currentHighestBidder;
+    Auctioneer auctioneer;
+    Strategy strategy;
 
+    void updateHighestBidder(float newHighestBidder)
+    {
+        currentHighestBidder = newHighestBidder;
+    }
+
+    void bid()
+    {
+        strategy.bid(auctioneer, this);
+    }
 }
--- src/auctioneer/HumanBidder.java
+++ src/auctioneer/HumanBidder.java
@@ -1,5 +1,25 @@
 package auctioneer;
 
-public class HumanBidder {
+public class HumanBidder implements Bidder{
+
+    private string name;
+    float currentHighestBidder;
+    Auctioneer auctioneer;
+    Strategy strategy;
+
+    void updateHighestBidder(float newHighestBidder)
+    {
+        currentHighestBidder = newHighestBidder;
+    }
+
+    void bid()
+    {
+        strategy.bid(auctioneer, this);
+    }
+
+    void bidCustom(float amount)
+    {
+        auctioneer.takeBid(amount, this)
+    }
 
 }
